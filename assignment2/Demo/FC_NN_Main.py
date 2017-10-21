cfbed2196481fc34088a99db7bd7f679c27a3a6b
Yu ShangLin <tjes50220@gmail.com>
2017-10-21 23:14:43 +0800

    Modular Fully Connected Network Main demo file update

--- assignment2/Demo/FC_NN_Main.py
+++ assignment2/Demo/FC_NN_Main.py
@@ -255,13 +255,13 @@ solver = Solver(model, small_data,
                   'learning_rate': learning_rate,
                 }
          )
-solver.train()
+#solver.train()
 
-plt.plot(solver.loss_history, 'o')
-plt.title('Training loss history')
-plt.xlabel('Iteration')
-plt.ylabel('Training loss')
-plt.show()
+#plt.plot(solver.loss_history, 'o')
+#plt.title('Training loss history')
+#plt.xlabel('Iteration')
+#plt.ylabel('Training loss')
+#plt.show()
 
 
 # TODO: Use a five-layer Net to overfit 50 training examples.
@@ -292,3 +292,85 @@ solver = Solver(model, small_data,
 #plt.ylabel('Training loss')
 #plt.show()
 
+##########################
+# Update rules
+##########################
+
+# SGD+Momentum
+
+from cs231n.optim import sgd_momentum
+
+N, D = 4, 5
+w = np.linspace(-0.4, 0.6, num=N*D).reshape(N, D)
+dw = np.linspace(-0.6, 0.4, num=N*D).reshape(N, D)
+v = np.linspace(0.6, 0.9, num=N*D).reshape(N, D)
+
+config = {'learning_rate': 1e-3, 'velocity': v}
+next_w, _ = sgd_momentum(w, dw, config=config)
+
+expected_next_w = np.asarray([
+  [ 0.1406,      0.20738947,  0.27417895,  0.34096842,  0.40775789],
+  [ 0.47454737,  0.54133684,  0.60812632,  0.67491579,  0.74170526],
+  [ 0.80849474,  0.87528421,  0.94207368,  1.00886316,  1.07565263],
+  [ 1.14244211,  1.20923158,  1.27602105,  1.34281053,  1.4096    ]])
+expected_velocity = np.asarray([
+  [ 0.5406,      0.55475789,  0.56891579, 0.58307368,  0.59723158],
+  [ 0.61138947,  0.62554737,  0.63970526,  0.65386316,  0.66802105],
+  [ 0.68217895,  0.69633684,  0.71049474,  0.72465263,  0.73881053],
+  [ 0.75296842,  0.76712632,  0.78128421,  0.79544211,  0.8096    ]])
+
+print 'next_w error: ', rel_error(next_w, expected_next_w)
+print 'velocity error: ', rel_error(expected_velocity, config['velocity'])
+
+num_train = 4000
+small_data = {
+  'X_train': data['X_train'][:num_train],
+  'y_train': data['y_train'][:num_train],
+  'X_val': data['X_val'],
+  'y_val': data['y_val'],
+}
+
+solvers = {}
+
+for update_rule in ['sgd', 'sgd_momentum']:
+  print 'running with ', update_rule
+  model = FullyConnectedNet([100, 100, 100, 100, 100], weight_scale=5e-2)
+
+  solver = Solver(model, small_data,
+                  num_epochs=5, batch_size=100,
+                  update_rule=update_rule,
+                  optim_config={
+                    'learning_rate': 1e-2,
+                  },
+                  verbose=True)
+  solvers[update_rule] = solver
+  solver.train()
+  print
+
+plt.subplot(3, 1, 1)
+plt.title('Training loss')
+plt.xlabel('Iteration')
+
+plt.subplot(3, 1, 2)
+plt.title('Training accuracy')
+plt.xlabel('Epoch')
+
+plt.subplot(3, 1, 3)
+plt.title('Validation accuracy')
+plt.xlabel('Epoch')
+
+for update_rule, solver in solvers.iteritems():
+  plt.subplot(3, 1, 1)
+  plt.plot(solver.loss_history, 'o', label=update_rule)
+  
+  plt.subplot(3, 1, 2)
+  plt.plot(solver.train_acc_history, '-o', label=update_rule)
+
+  plt.subplot(3, 1, 3)
+  plt.plot(solver.val_acc_history, '-o', label=update_rule)
+  
+for i in [1, 2, 3]:
+  plt.subplot(3, 1, i)
+  plt.legend(loc='upper center', ncol=4)
+plt.gcf().set_size_inches(15, 15)
+plt.show()
